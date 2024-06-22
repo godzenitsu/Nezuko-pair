fb8326d79831628fb363384a5f8de04ad40ede6b
GOD ZENITSU <164893015+godzenitsu@users.noreply.github.com>
2024-06-22 08:52:04 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 module.exports = {
-  MONGODB_URL: process.env.MONGODB_URL || "mongodb+srv://godzenitsu:godzenitsu@queen-nezuko.dzvsybz.mongodb.net/?retryWrites=true&w=majority&appName=queen-nezuko",
-  SESSION_NAME: process.env.SESSION_NAME || "Queen~Nezuko_"
+  MONGODB_URL: process.env.MONGODB_URL || "mongodb+srv://hunternight691:wfgasSwGcbQnkKD1@cluster0.pvilxll.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
+  SESSION_NAME: process.env.SESSION_NAME || "Queen_Nezuko_"
 };
